c8c8be80afb9c57ac483bc2bcebdbdf5816b61a5
yakov <jasha3131@gmail.com>
2021-07-22 13:35:13 +0500

    Настроил конфигурацию

--- lego_car_test/settings/base.py
+++ lego_car_test/settings/base.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'lego_car_test.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, '../../db.sqlite3'),
     }
 }
 
@@ -120,6 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static_debug'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_release')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
---
+++ lego_car_test/settings/production.py
@@ -0,0 +1,13 @@
+from .base import *
+
+TIME_ZONE = 'Europe/Moscow'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB_NAME'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': os.environ.get('POSTGRES_HOST'),
+    }
+}
