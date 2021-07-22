27ef1de7f41542186dfcc665e5bf5890c08707a8
yakov <jasha3131@gmail.com>
2021-07-22 13:56:24 +0500

    Правка файла Readme.md

--- lego_car_test/settings/production.py
+++ lego_car_test/settings/production.py
@@ -1,13 +1,14 @@
 from .base import *
 
 TIME_ZONE = 'Europe/Moscow'
+DEBUG = False
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('POSTGRES_DB_NAME'),
-        'USER': os.environ.get('POSTGRES_USER'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'HOST': os.environ.get('POSTGRES_HOST'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.environ.get('POSTGRES_DB_NAME'),
+#         'USER': os.environ.get('POSTGRES_USER'),
+#         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+#         'HOST': os.environ.get('POSTGRES_HOST'),
+#     }
+# }
